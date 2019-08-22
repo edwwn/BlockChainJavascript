c496cf6a19910700d3a61806579a53e70d71db00
edwwn <40034470+edwwn@users.noreply.github.com>
2019-08-22 11:04:23 +0300

    Imported the Hash file and Installed Code for Hash

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,3 +1,6 @@
+const sha256 = require('sha256');
+
+
 function Blockchain() {
     this.chain = [];
     this.pendingTransactions = [];
@@ -33,6 +36,13 @@ Blockchain.prototype.createNewTransaction = function (
 
     return this.getLastBlock()['index'] + 1;
 };
+
+
+Blockchain.prototype.hashBlock = function (previuosBlockhash, currentBlockData, nounce) {
+    const dataAsString = previuosBlockhash + nonce.toString() + JSON.stringify(currentBlockData);
+    const hash = sha256(dataAsString);
+    return hash;
+}
 
 module.exports = Blockchain;
 
--- dev/test.js
+++ dev/test.js
@@ -1,12 +1,19 @@
 const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
-bitcoin.createNewBlock(2434, 'EQWUIEYQWUYHEUIQWB', 'SAKJDLASHFKLASHFASFJA')
-bitcoin.createNewBlock(2434, 'EQWUDFGSGTTHYGHQWB', 'SAGHJFKJDLASHFKLASHFASFJA')
-bitcoin.createNewBlock(24534, 'EQWUIEYQWUYHGFHEUIQWB', 'SAFGHFGJERWGKJDLASHFKLASHFASFJA')
+bitcoin.createNewBlock(2434, 'EQWUIEYQWUYHEUIQWB', 'SAKJDLASHFKLASHFASFJA');
 
-bitcoin.createNewTransaction(100, 'ALEXfhskldfjleoutuoe', 'JANEhfjkashjkfhsue')
+//bitcoin.createNewBlock(24534, 'EQWUIEYQWUYHGFHEUIQWB', 'SAFGHFGJERWGKJDLASHFKLASHFASFJA')
+
+bitcoin.createNewTransaction(100, 'ALEXfhskldfjleoutuoe', 'JANEhfjkashjkfhsue');
+
+bitcoin.createNewBlock(56678, 'TRHGHGJHYJHJ', 'GJGHJHJYUIKII');
+
+bitcoin.createNewTransaction(67, 'ALEXfhskldfjleoutuoe', 'JANEhfjkashjkfhsue');
+bitcoin.createNewTransaction(898, 'ALEXfhskldfjleoutuoe', 'JANEhfjkashjkfhsue');
+bitcoin.createNewTransaction(5655, 'ALEXfhskldfjleoutuoe', 'JANEhfjkashjkfhsue');
+
+bitcoin.createNewBlock(546567, 'FGJGHJGHJHJ', 'WEQREWRYTRYTWY');
 
 
-
-console.log(bitcoin);
+console.log(bitcoin.chain[2]);
