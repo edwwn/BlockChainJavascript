c58edb56f43019c3a17dc9c69c249e354907729b
edwwn <40034470+edwwn@users.noreply.github.com>
2019-08-22 09:25:53 +0300

    tested the pendingTranscation

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,23 +1,41 @@
 function Blockchain() {
     this.chain = [];
-    this.newTransactions = [];
+    this.pendingTransactions = [];
 }
-
 
 Blockchain.prototype.createNewBlock = function (nonce, previuosBlockhash, hash) {
     const newBlock = {
         index: this.chain.length + 1,
         timestamp: Date.now(),
-        transactions: this.newTransactions,
+        transactions: this.pendingTransactions,
         nonce: nonce,
         hash: hash,
         previuosBlockhash: previuosBlockhash
     };
 
-    this.newTransactions = [];
+    this.pendingTransactions = [];
     this.chain.push(newBlock);
 
     return newBlock;
-}
+};
+
+Blockchain.prototype.createNewTransaction = function (
+    amount,
+    sender,
+    recipient
+) {
+    const newTransaction = {
+        amount: amount,
+        sender: sender,
+        recipient: recipient
+    };
+    this.pendingTransactions.push(newTransaction);
+
+    return this.getLastBlock()['index'] + 1;
+};
 
 module.exports = Blockchain;
+
+Blockchain.prototype.getLastBlock = function () {
+    return this.chain[this.chain.length - 1];
+};
--- dev/test.js
+++ dev/test.js
@@ -5,4 +5,8 @@ bitcoin.createNewBlock(2434, 'EQWUIEYQWUYHEUIQWB', 'SAKJDLASHFKLASHFASFJA')
 bitcoin.createNewBlock(2434, 'EQWUDFGSGTTHYGHQWB', 'SAGHJFKJDLASHFKLASHFASFJA')
 bitcoin.createNewBlock(24534, 'EQWUIEYQWUYHGFHEUIQWB', 'SAFGHFGJERWGKJDLASHFKLASHFASFJA')
 
+bitcoin.createNewTransaction(100, 'ALEXfhskldfjleoutuoe', 'JANEhfjkashjkfhsue')
+
+
+
 console.log(bitcoin);
