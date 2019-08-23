33c68a61c015f2e0d3e6decbf3e3715343cc700a
edwwn <40034470+edwwn@users.noreply.github.com>
2019-08-23 10:08:45 +0300

    register-node endpoint

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -94,7 +94,13 @@ app.post('/register-and-brocast-node', function (req, res) {
 
             // register a node with network
             app.post('/register-node', function (req, res) {
-
+                const newNodeUrl = req.body.newNodeUrl;
+                const nodeNotAllreadyPresent = bitcoin.networkNodeUrl.indexOf(newNodeUrl) == -1;
+                const notCurrentNode = bitcoin.currentNodeUrl !== newNodeUrl;
+                if (nodeNotAllreadyPresent && notCurrentNode) bitcoin.networkNodes.push(newNodeUrl);
+                res.json({
+                    note: 'New node Registed Sucessfully'
+                });
             });
 
 
